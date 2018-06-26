7b4169ac73a8e7bef8332dba80721f4851e4fa74
crypto-jones <integral.being@gmail.com>
2018-06-26 14:57:18 -0700

    complete MVP for Tuesday

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 
 class Bookmark(models.Model):
@@ -9,3 +10,7 @@ class Bookmark(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     category = models.CharField(max_length=20)
+
+
+class PersonalBookmark(Bookmark):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
